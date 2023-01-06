b8cc295ec1ba14eab0c7d172289ed90de50dcf95
Jonhathan HO TZU YU <hotzuyu@hotzuyu-graphic.fr>
2023-01-06 16:19:01 +0100

    hero style

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,11 +1,9 @@
 import React from "react"
 import "../../utils/css/style.css"
-import image from "../../assets/background-image_home.jpg"
 
 function Hero() {
     return(
-        <div className="hero" style={{
-            backgroundImage: `url(${image})` }}>
+        <div className="hero">
             <h1 className="hero__title">Chez vous, partout et ailleurs</h1>
         </div>
     )
