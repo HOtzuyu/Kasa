529a521b540c3e9c8d9e75758c2e563a5b2cc1b5
Jonhathan HO TZU YU <hotzuyu@hotzuyu-graphic.fr>
2023-01-08 16:44:46 +0100

    ajustement image

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 import "../../utils/css/style.css"
+import Image from "../../assets/backgroundHome.jpg"
 
 function Hero() {
     return(
         <div className="hero">
+            <img className="hero__image" src={ Image } alt="image du header" />
             <h1 className="hero__title">Chez vous, partout et ailleurs</h1>
         </div>
     )
